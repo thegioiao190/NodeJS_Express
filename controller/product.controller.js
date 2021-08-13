29b4a58fa32c9313cfdacabfddc79545f7f15d6c
MinPink <thegioiao@gmail.com>
2021-08-13 13:46:14 +0700

    Bai 20 Phan trang

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,5 +1,19 @@
 var db = require("../db");
 
 module.exports.showProduct = function(req,res,next){
-	res.render("product",{db:db.db.products});
+	var page = parseInt(req.query.page) || 1
+	var itemPerPage = 9
+	var begin = (page-1)*itemPerPage;
+	var end = page * itemPerPage;
+
+	var DB = db.db.products.slice(begin,end);
+	res.render("product",{
+							db:DB,
+							page:page
+						});
 }
+//Coong thuc
+//  n = so tu tu trang
+//  x = so item moi trang
+// begin = (n-1)*x
+// end   = n*x
